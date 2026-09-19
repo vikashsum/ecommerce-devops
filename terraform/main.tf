61175e64595abc240282bbf9d0672cde75b7379f
vikashsum <vikashsuman886@gmail.com>
2026-09-19 14:12:14 +0530

    feat: add GitHub OIDC for AWS ECR (#4)

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,6 +8,7 @@ resource "aws_vpc" "main" {
     Project = var.project_name
   }
 }
+
 resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.101.0/24"
@@ -53,6 +54,7 @@ resource "aws_subnet" "private_2" {
     Project = var.project_name
   }
 }
+
 resource "aws_ecr_repository" "backend" {
   name                 = "${var.project_name}-backend"
   image_tag_mutability = "IMMUTABLE"
@@ -65,4 +67,80 @@ resource "aws_ecr_repository" "backend" {
     Name    = "${var.project_name}-backend"
     Project = var.project_name
   }
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
+}
+
+resource "aws_iam_role" "github_actions" {
+  name = "${var.project_name}-github-actions"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+
+        Action = "sts:AssumeRoleWithWebIdentity"
+
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+
+            "token.actions.githubusercontent.com:sub" = "repo:vikashsum@241154818/ecommerce-devops@1375722798:ref:refs/heads/develop"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "github_actions_ecr" {
+  name = "${var.project_name}-github-actions-ecr"
+  role = aws_iam_role.github_actions.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ecr:GetAuthorizationToken"
+        ]
+
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:CompleteLayerUpload",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImage",
+          "ecr:UploadLayerPart"
+        ]
+
+        Resource = aws_ecr_repository.backend.arn
+      }
+    ]
+  })
 }
